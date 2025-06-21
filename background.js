854395e491b367561744f1a0e8238d1769f43ba8
Aditya Singh Bisht <adityasingbisht100@gmail.com>
2025-06-21 12:54:39 +0530

    First Commit

---
+++ background.js
@@ -0,0 +1,6 @@
+// background.js
+// Author: Aditya Singh Bisht
+// Author Github URI: https://github.com/AdityaSinghBisht100
+// Project Repository URI:https://github.com/AdityaSinghBisht100/Chrome-Extension-AlgoZenith
+// Description: This project is a chrome extension in which an AI-generated chatbot interacts with user to solve the user queries related to the algorithm problem (It only works in maang.in website).
+// License: MIT
